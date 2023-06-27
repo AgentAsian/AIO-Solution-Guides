b96fa8f235809ab13fe683a7401177fd8d0eee2a
AgentAsian <112794384+AgentAsian@users.noreply.github.com>
2023-06-27 21:02:27 +1000

    Create 0template.cpp

---
+++ 0template.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#define speed() \
+ios::sync_with_stdio(false); \
+cin.tie(0)
+#define readfile(name) \
+speed(); \
+freopen(#name "in.txt","r",stdin); \
+freopen(#name "out.txt","w",stdout)
+using namespace std;
+
+int main() {
+    readfile(name);
+}
